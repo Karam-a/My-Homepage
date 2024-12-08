ded71ca3c44678e8e88b5e24de1b236c3150c4d7
Karam-a <81566216+Karam-a@users.noreply.github.com>
2024-12-08 22:13:15 +0100

    Recreated from earlier repo
    Caused by issues with earlier repo. Recreated what was made until that point in this repo.

---
+++ my-homepage/src/assets/icons/index.js
@@ -0,0 +1,23 @@
+import userIcon from "./userIcon.svg";
+import briefcaseIcon from "./briefcaseIcon.svg";
+import codeIcon from "./codeIcon.svg";
+import mailIcon from "./mailIcon.svg";
+import githubIcon from "./githubIcon.svg";
+import linkedinIcon from "./linkedinIcon.svg";
+import linkIcon from "./linkIcon.svg";
+import sunIcon from "./sunIcon.svg";
+import moonIcon from "./moonIcon.svg";
+
+export const Icons = {
+    User: userIcon,
+    Briefcase: briefcaseIcon,
+    Code: codeIcon, 
+    Mail: mailIcon,
+    Github: githubIcon,
+    LinkedIn: linkedinIcon,
+    ExternalLink: linkIcon,
+    Sun: sunIcon,
+    Moon: moonIcon
+}
+
+export default Icons;
---
+++ my-homepage/src/components/icon.vue
@@ -0,0 +1,65 @@
+<!-- Komponent som hanterar ikonrelaterad logik-->
+
+<!-- Ikontemplate-->
+<template>
+    <span
+    class="inline-flex"
+    :class="[sizeClass, colorClass]"
+    v-html="iconSvg"></span>
+</template>
+
+<!-- Inhämtning av ikonsamlingen. Anger att namnet på komponenten är "icon" samt definierar/skapar tre properties för ikonerna; namn, size och color -->
+<script>
+import {Icons} from "../assets/icons";
+
+const IconSizes = {
+    sm: 'w-4 h-4',
+    md: 'w-6 h-6',
+    lg: 'w-8 h-8',
+    xl: 'w-10 h-10'
+    };
+
+const IconColors = {
+    current: 'text-current',
+    prim: 'text-blue-500',
+    sec: 'text-purple-500',
+    gray: 'text-gray-500'
+    };
+
+export default {
+    name: "Icon",
+    props: {
+        name: {
+            type: String
+        }, 
+        size: {
+            type: String, 
+            default: 'md', //Finns som alternativ; sm (16px), md (24px), lg (32px), xl(40px)
+
+        }, 
+        color: {
+            type: String,
+            default: "current" //Finns som alternativen: Current, prim (blue-500), sec (purple-500) & gray (gray-500)
+        }
+    },
+    
+    computed: {
+        iconSvg() {//Tar namn som property, hittar matchande ikon.svg från Icons-objektet. Returnerar en tom string om objektet inte kan hittas.
+            return Icons[this.name] || '';
+        }, 
+        sizeClass(){ 
+            return IconSizes[this.size];
+        }, 
+        colorClass(){
+            return IconColors[this.color];
+        }
+
+    }
+};
+</script>
+
+<style scoped lang="postcss">
+span :deep(svg){
+    @apply w-full h-full;
+}
+</style>
